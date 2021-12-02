b626f91b7cbc4674979b29d29d847d2ca009c893
Fiqrian <fiqrian.faturahman@gmail.com>
2021-12-02 21:49:42 +0700

    first commit

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+const Tugas19 = () => {
+  let x = "Semangat....";
+  console.log(x.repeat(5));
+};
+
+Tugas19();
+console.log("___________________________");
+
+const Repeat = () => {
+  let x = "*";
+  console.log(x.repeat(5));
+  console.log(x.repeat(20));
+  console.log(x.repeat(50));
+  console.log(x.repeat(100));
+  console.log(x.repeat(120));
+  console.log(x.repeat(100));
+  console.log(x.repeat(50));
+  console.log(x.repeat(20));
+  console.log(x.repeat(5));
+};
+Repeat();
